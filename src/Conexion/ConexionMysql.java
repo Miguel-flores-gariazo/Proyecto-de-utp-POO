4f0b7b79681d70fb883a8227ed0db64a82718ff5
KevinEsTrada21 <108201024+KevinEsTrada21@users.noreply.github.com>
2024-06-14 07:57:54 -0500

    Update ConexionMysql.java

--- src/Conexion/ConexionMysql.java
+++ src/Conexion/ConexionMysql.java
@@ -1,9 +1,20 @@
 package Conexion;
 
 import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
 
 public class ConexionMysql {
     Connection cn;
             
-            
+    public Connection conectar(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn=(Connection) DriverManager.getConnection("jdbc:mysql://localhost/plazavea","root","");
+            System.out.println("Conexcion Establecida");
+        } catch (Exception e) {
+            System.out.println("ERROR DE CONEXION"+e);
+        }
+        return cn;
+    }
 }
+
