a736e1a70bed8467ddfba57d1b20fd28d04c648b
Miguel-flores-gariazo <miguelfloresgariazo@gmail.com>
2024-07-05 09:39:33 -0500

    Add files via upload

---
+++ src/plazavea/solicitudseguro/SolicitudSeguroSalud.java
@@ -0,0 +1,159 @@
+package plazavea.solicitudseguro;
+
+public class SolicitudSeguroSalud {
+    // Datos de la entidad empleadora/responsable
+    private String razonSocial;
+    private String ruc;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String nombres;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private String fechaNacimiento;
+    private String sexo;
+    private String estadoCivil;
+    private String numeroTelefono;
+    private String email;
+    private String direccion;
+
+    public SolicitudSeguroSalud(String razonSocial, String ruc, String apellidoPaterno, String apellidoMaterno, String nombres, String tipoDocumento, String numeroDocumento, String fechaNacimiento, String sexo, String estadoCivil, String numeroTelefono, String email, String direccion) {
+        this.razonSocial = razonSocial;
+        this.ruc = ruc;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.nombres = nombres;
+        this.tipoDocumento = tipoDocumento;
+        this.numeroDocumento = numeroDocumento;
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+        this.numeroTelefono = numeroTelefono;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+ 
+   
+    @Override
+    public String toString() {
+        return "SolicitudSeguroSalud{" +
+                "razonSocial='" + razonSocial + '\'' +
+                ", ruc='" + ruc + '\'' +
+                ", apellidoPaterno='" + apellidoPaterno + '\'' +
+                ", apellidoMaterno='" + apellidoMaterno + '\'' +
+                ", nombres='" + nombres + '\'' +
+                ", tipoDocumento='" + tipoDocumento + '\'' +
+                ", numeroDocumento='" + numeroDocumento + '\'' +
+                ", fechaNacimiento='" + fechaNacimiento + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", estadoCivil='" + estadoCivil + '\'' +
+                ", numeroTelefono='" + numeroTelefono + '\'' +
+                ", email='" + email + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
+}
